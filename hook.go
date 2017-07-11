88ed1c8a54ce0f01be58672a91aebe579db1b1c0
Qi Xiao <xiaqqaix@gmail.com>
2017-07-11 22:31:40 +0200

    Make the Messenger build messages.

--- hook.go
+++ hook.go
@@ -80,66 +80,26 @@ func eventToMessage(eventType string, req []byte, m Messenger) {
 		log.Println("pinged")
 	case "push":
 		var event PushEvent
-		if !parse(&event) {
-			return
-		}
-		m.Messagef("@%s pushed %s to %s:",
-			event.Sender.Login,
-			withNum(len(event.Commits), "commit", "commits"),
-			humanizeRef(event.Ref))
-		for _, commit := range event.Commits {
-			m.Messagef("  %s (by %s)",
-				firstLine(commit.Message), commit.Author.Name)
+		if parse(&event) {
+			m.OnPush(event)
 		}
 	case "issues":
 		var event IssuesEvent
-		if !parse(&event) {
-			return
+		if parse(&event) {
+			m.OnIssues(event)
 		}
-		switch event.Action {
-		case "opened", "closed":
-			m.Messagef("@%s %s issue #%d (%s)",
-				event.Sender.Login, event.Action,
-				event.Issue.Number, event.Issue.Title)
-		default:
-			log.Println("ignored issue being", event.Action)
-		}
-		return
 	case "issue_comment":
 		var event IssueCommentEvent
-		if !parse(&event) {
-			return
-		}
-		switch event.Action {
-		case "created":
-			m.Messagef("@%s commented on issue #%d (%s):",
-				event.Sender.Login, event.Issue.Number, event.Issue.Title)
-			m.Messagef("  %s", abbrComment(event.Comment.Body))
-		default:
-			log.Println("ignored issue comment being", event.Action)
-			return
+		if parse(&event) {
+			m.OnIssueComment(event)
 		}
 	case "pull_request":
 		var event PullRequestEvent
-		if !parse(&event) {
-			return
-		}
-		switch event.Action {
-		case "opened", "closed", "reopened":
-			action := event.Action
-			if action == "closed" && event.PullRequest.Merged {
-				action = "merged"
-			}
-			m.Messagef("@%s %s pull request #%d (%s)",
-				event.Sender.Login, action,
-				event.PullRequest.Number, event.PullRequest.Title)
-		default:
-			log.Println("ignored pull request being", event.Action)
-			return
+		if parse(&event) {
+			m.OnPullRequest(event)
 		}
 	default:
 		log.Println("ignored event", eventType)
-		return
 	}
 }
 
--- messenger.go
+++ messenger.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 type Messenger interface {
-	Message(string)
-	Messagef(string, ...interface{})
+	OnPush(PushEvent)
+	OnIssues(IssuesEvent)
+	OnIssueComment(IssueCommentEvent)
+	OnPullRequest(PullRequestEvent)
 }
 
 type IRCMessenger struct {
@@ -12,10 +17,58 @@ type IRCMessenger struct {
 	Channel string
 }
 
-func (im *IRCMessenger) Message(msg string) {
-	im.Sendf("PRIVMSG #%s :%s", im.Channel, msg)
+func (m *IRCMessenger) Message(msg string) {
+	m.Sendf("PRIVMSG #%s :%s", m.Channel, msg)
 }
 
-func (im *IRCMessenger) Messagef(s string, a ...interface{}) {
-	im.Message(fmt.Sprintf(s, a...))
+func (m *IRCMessenger) Messagef(s string, a ...interface{}) {
+	m.Message(fmt.Sprintf(s, a...))
+}
+
+func (m *IRCMessenger) OnPush(event PushEvent) {
+	m.Messagef("@%s pushed %s to %s:",
+		event.Sender.Login,
+		withNum(len(event.Commits), "commit", "commits"),
+		humanizeRef(event.Ref))
+	for _, commit := range event.Commits {
+		m.Messagef("  %s (by %s)",
+			firstLine(commit.Message), commit.Author.Name)
+	}
+}
+
+func (m *IRCMessenger) OnIssues(event IssuesEvent) {
+	switch event.Action {
+	case "opened", "closed":
+		m.Messagef("@%s %s issue #%d (%s)",
+			event.Sender.Login, event.Action,
+			event.Issue.Number, event.Issue.Title)
+	default:
+		log.Println("ignored issue being", event.Action)
+	}
+}
+
+func (m *IRCMessenger) OnIssueComment(event IssueCommentEvent) {
+	switch event.Action {
+	case "created":
+		m.Messagef("@%s commented on issue #%d (%s):",
+			event.Sender.Login, event.Issue.Number, event.Issue.Title)
+		m.Messagef("  %s", abbrComment(event.Comment.Body))
+	default:
+		log.Println("ignored issue comment being", event.Action)
+	}
+}
+
+func (m *IRCMessenger) OnPullRequest(event PullRequestEvent) {
+	switch event.Action {
+	case "opened", "closed", "reopened":
+		action := event.Action
+		if action == "closed" && event.PullRequest.Merged {
+			action = "merged"
+		}
+		m.Messagef("@%s %s pull request #%d (%s)",
+			event.Sender.Login, action,
+			event.PullRequest.Number, event.PullRequest.Title)
+	default:
+		log.Println("ignored pull request being", event.Action)
+	}
 }
